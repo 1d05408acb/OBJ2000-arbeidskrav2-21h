af165786d5e9a9b410b6facc4e0e08bc08e57d48
Bjørnar Borge <dev.b.borge@gmail.com>
2021-10-31 21:49:25 +0100

    Create Kontroll.java

---
+++ src/arbeidskrav2/Kontroll.java
@@ -0,0 +1,20 @@
+package arbeidskrav2;
+
+import java.util.ArrayList;
+
+public class Kontroll {
+    // Opprette en array liste
+    ArrayList<Dyr> liste = new ArrayList<Dyr>();
+    // Opprette variabler for å kunne telle
+    private int antallGauper = 0;
+    private int antallHarer = 0;
+
+    public void registrerGaupe(String kjonn, double lengde, double vekt, String sted, String dato, double lengdeOre){
+        antallGauper++; // Hver gang det loopes igjennom så legges til ett tall til variabelen
+        String id = "G" + antallGauper;
+        id.trim(); // Fjerner mellomrom mellom string og int
+        Gaupe nyGaupe = new Gaupe(id, kjonn, lengde, vekt, sted, dato, lengdeOre);
+        liste.add(nyGaupe);
+    }
+
+}
