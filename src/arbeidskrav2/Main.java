7844237e22aa38dfd0cea365fa7760ccc96aabef
Bjørnar Borge <dev.b.borge@gmail.com>
2021-10-31 21:49:29 +0100

    Create Main.java

---
+++ src/arbeidskrav2/Main.java
@@ -0,0 +1,9 @@
+package arbeidskrav2;
+
+public class Main {
+    public static void main(String[] args) {
+	// Koble mot grensesnittet (GUI) klassen
+        GUI grensesnitt = new GUI();
+        grensesnitt.program();
+    }
+}
