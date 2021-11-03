46af73ed6262c6ab786061b30af1f05fee63b04c
Bjørnar Borge <dev.b.borge@gmail.com>
2021-11-03 10:00:42 +0100

    Update Gjenfangst.java

--- src/arbeidskrav2/Gjenfangst.java
+++ src/arbeidskrav2/Gjenfangst.java
@@ -1,4 +1,39 @@
 package arbeidskrav2;
 
 public class Gjenfangst {
+    private String dato;
+    private String sted;
+    private double lengde;
+    private double vekt;
+
+    public Gjenfangst(String d, String s, double l, double v) {
+        dato = d;
+        sted = s;
+        lengde = l;
+        vekt = v;
+    }
+
+    public String hentDato() {
+        return dato;
+    }
+
+    public String hentSted() {
+        return sted;
+    }
+
+    public double hentLengde() {
+        return lengde;
+    }
+
+    public double hentVekt() {
+        return vekt;
+    }
+
+    public String toString() {
+        return "Dato for gjenfangst: " + dato + "\n" +
+                "Sted: " + sted + "\n" +
+                "Lengde: " + lengde + "\n" +
+                "Vekt: " + vekt;
+    }
+
 }
