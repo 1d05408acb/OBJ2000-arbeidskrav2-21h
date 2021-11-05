96d1e1b53e3010ebddaa91b3c9040f332ff954e8
Bjørnar Borge <dev.b.borge@gmail.com>
2021-11-05 21:03:39 +0100

    Update GjenfangstGaupe.java

--- src/arbeidskrav2/GjenfangstGaupe.java
+++ src/arbeidskrav2/GjenfangstGaupe.java
@@ -3,8 +3,8 @@ package arbeidskrav2;
 public class GjenfangstGaupe extends Gjenfangst {
     double lengdeOre;
 
-    public GjenfangstGaupe(String dato, String sted, double lengde, double vekt, double lengdeOre) {
-        super(dato, sted, lengde, vekt);
+    public GjenfangstGaupe(String id, String dato, String sted, double lengde, double vekt, double lengdeOre) {
+        super(id, dato, sted, lengde, vekt);
         this.lengdeOre = lengdeOre;
     }
 }
