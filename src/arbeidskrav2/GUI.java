db2e59401776015e3a8961eb648b238c432f106b
Bjørnar Borge <dev.b.borge@gmail.com>
2021-11-05 21:03:43 +0100

    Update GUI.java

--- src/arbeidskrav2/GUI.java
+++ src/arbeidskrav2/GUI.java
@@ -3,20 +3,22 @@ package arbeidskrav2;
 import java.util.ArrayList;
 import javax.swing.JOptionPane;
 
+import static arbeidskrav2.Dyr.listeGjenfangst;
+
 public class GUI {
+    // Alternativer / valg i menyen som aktiveres med case
     Kontroll kontroll = new Kontroll();
-    // Alternativer / valg i menyen
     private final String[] ALTERNATIVER =
             {"Registrer gaupe",
             "Registrer hare",
             "Registrer gjenfangst gaupe",
             "Registrer gjenfangst hare",
-            "Finn ett dyr",
+            "Finn ett dyr:",
             "Skriv ut dyre liste",
             "Dyr med fangst",
             "Avslutt"};
 
-    // Kode for å motta valg (integer input) fra knapper
+    // Kode for å motta valg (integer input) fra knapper og melding på hovedsiden + tittel på vinduet
     public int valgt() {
         int valg;
         valg = JOptionPane.showOptionDialog(null,
@@ -31,6 +33,7 @@ public class GUI {
     }
 
     // Kode for å aktivere ulike funksjoner ved valg av knapper
+    // Bruker while løkke som avsluttes hvis brukeren trykket på noe annet enn 0-6 som er valg 0-6
     public void program() {
         boolean fortsette = true;
         while(fortsette) {
@@ -48,6 +51,7 @@ public class GUI {
         }
     }
 
+    // Lager registrer gaupe funksjon som tar imot input og sender videre til registrerGaupe i kontroll klassen.
     public void registrerGaupe() {
         String kjonn = JOptionPane.showInputDialog("Gaupen's kjønn (hann/hunn): ");
         double lengde = Double.parseDouble(JOptionPane.showInputDialog("Gaupen's lengde i cm: "));
@@ -58,6 +62,7 @@ public class GUI {
         kontroll.registrerGaupe(kjonn, lengde, vekt, sted, dato, lengdeOre);
     }
 
+    // Lager registrer hare funksjon som tar imot input og sender videre til registrerHare i kontroll klassen
     public void registrerHare() {
         String kjonn = JOptionPane.showInputDialog("Haren's kjønn (hann/hunn): ");
         double lengde = Double.parseDouble(JOptionPane.showInputDialog("Haren's lengde i cm: "));
@@ -65,12 +70,13 @@ public class GUI {
         String sted = JOptionPane.showInputDialog("Sted fanget: ");
         String dato = JOptionPane.showInputDialog("Dato fanget (dd.mm.yyyy): ");
         String type = JOptionPane.showInputDialog("Haren's type (vanlig hare/sørhare): ");
-        char type_formatert = type.charAt(0);
+        char type_formatert = type.charAt(0); //formatere til å kun ta imot bokstav/char på plass 0 (første bokstav) i stringen
         String farge = JOptionPane.showInputDialog("Haren's farge (hvit/brun): ");
-        char farge_formatert = farge.charAt(0);
+        char farge_formatert = farge.charAt(0); //formatere til å kun ta imot bokstav/char på plass 0 (første bokstav) i stringen
         kontroll.registrerHare(kjonn, lengde, vekt, sted, dato, type_formatert, farge_formatert);
     }
 
+    // Lager registrer gjenfangst gaupe funksjon som tar imot input og sender videre til registrerGjenfangstGaupe i kontroll klassen
     public void registrerGjennfangstGaupe() {
         String id = JOptionPane.showInputDialog("Dyrets ID: ");
         double lengde = Double.parseDouble(JOptionPane.showInputDialog("Gaupen's lengde i cm: "));
@@ -81,6 +87,7 @@ public class GUI {
         kontroll.registrerGjenfangstGaupe(id, dato, sted, lengde, vekt, lengdeOre);
     }
 
+    // Lager registrer gjenfangst hare funksjon som tar imot input og sender videre til registrerGjenfangstHare i kontroll klassen
     public void registrerGjennfangstHare() {
         String id = JOptionPane.showInputDialog("Dyrets ID: ");
         double lengde = Double.parseDouble(JOptionPane.showInputDialog("Haren's lengde i cm: "));
@@ -92,17 +99,28 @@ public class GUI {
         kontroll.registrerGjenfangstHare(id, dato, sted, lengde, vekt, farge_formatert);
     }
 
+    // Lage finn dyr funksjon som tar imot input på dyrets ID. Hvor den så kobler seg mot kontroll klassen og finnDyr hvor den sender "id" parameteren
     public void finnDyr() {
         String id = JOptionPane.showInputDialog("Dyrets ID: ");
-        id = id.trim();
-        Dyr Dyret = kontroll.finnDyr(id);
-        if (Dyret != null) {
-            JOptionPane.showMessageDialog(null, Dyret.toString());
-        } else {
-            JOptionPane.showMessageDialog(null, "Fant ikke dyret");
+        StringBuilder tekst = new StringBuilder();
+        Dyr DyretBinaert = kontroll.finnDyrBinaert(id);
+        if (DyretBinaert != null) {
+            tekst.append(DyretBinaert);
+            tekst.append("\n");
+            tekst.append("------------------------");
+            tekst.append("\n");
+            /*Gjenfangst DyretLinjaert = kontroll.finnDyrLinjaert(id);
+            tekst.append(DyretLinjaert);
+            tekst.append("\n");
+            tekst.append("------------------------");
+            tekst.append("\n"); Har kommentert ut koden her siden jeg ikke klarte å koble det opp mot linjær søket i kontroll*/
+            } else {
+                JOptionPane.showMessageDialog(null, "Fant ingen dyr med den ID'en");
         }
+        JOptionPane.showMessageDialog(null, tekst.toString());
     }
 
+    // Liste ut dyr uten gjenfangst
     public void listDyr() {
         ArrayList<Dyr> Dyrene = kontroll.getDyr();
         StringBuilder tekst = new StringBuilder();
@@ -115,6 +133,7 @@ public class GUI {
         JOptionPane.showMessageDialog(null, tekst.toString());
     }
 
+    // Liste ut dyr + gjenfangst
     public void listDyrMedGjenfangst() {
         ArrayList<Dyr> Dyrene = kontroll.getDyr();
         StringBuilder tekst = new StringBuilder();
@@ -123,7 +142,7 @@ public class GUI {
             tekst.append("\n");
             tekst.append("------------------------");
             tekst.append("\n");
-            for (Gjenfangst g : Dyret.listeGjenfangst) {
+            for (Gjenfangst g : listeGjenfangst) {
                 tekst.append(g.toString());
                 tekst.append("\n");
                 tekst.append("------------------------");
