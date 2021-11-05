129780000496ff2ebf3d788e245fb49af1dea942
Bjørnar Borge <dev.b.borge@gmail.com>
2021-11-05 21:03:46 +0100

    Update Main.java

--- src/arbeidskrav2/Main.java
+++ src/arbeidskrav2/Main.java
@@ -3,7 +3,7 @@ package arbeidskrav2;
 public class Main {
     public static void main(String[] args) {
 	// Koble mot grensesnittet (GUI) klassen
-        GUI grensesnitt = new GUI();
-        grensesnitt.program();
+        GUI grensesnitt = new GUI(); // Lager ett grensesnitt objekt
+        grensesnitt.program(); // Kaller på grensesnitt klassen
     }
 }
