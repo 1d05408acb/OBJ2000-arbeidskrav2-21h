54098045e5c201a841f964dcb639752db9654261
Bjørnar Borge <dev.b.borge@gmail.com>
2021-11-05 10:24:29 +0100

    Update Hare.java

--- src/arbeidskrav2/Hare.java
+++ src/arbeidskrav2/Hare.java
@@ -2,21 +2,13 @@ package arbeidskrav2;
 
 // Lage en subklasse Hare av Dyr
 public class Hare extends Dyr {
-    private char typen; // Initiere variabel
-    private char fargen; // Initiere variabel
+    private final char typen; // Initiere variabel
+    private final char fargen; // Initiere variabel
 
     public Hare(String id, String kjonn, double lengde, double vekt, String sted, String dato, char type, char farge) {
         super(id, kjonn, lengde, vekt, sted, dato);
         typen = type;
         fargen = farge;
-    }
-
-    public char hentType() {
-        return typen;
-    }
-
-    public char hentFarge() {
-        return fargen;
     }
 
     public String toString() {
