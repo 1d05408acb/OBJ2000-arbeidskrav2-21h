68e991e6488712a8e9f368b4c6235b33ffe2eba5
Bjørnar Borge <dev.b.borge@gmail.com>
2021-11-05 21:03:37 +0100

    Update Gjenfangst.java

--- src/arbeidskrav2/Gjenfangst.java
+++ src/arbeidskrav2/Gjenfangst.java
@@ -1,16 +1,29 @@
 package arbeidskrav2;
 
+import java.util.ArrayList;
+
 public class Gjenfangst {
+    public static ArrayList<Gjenfangst> listeGjenfangst = new ArrayList<>();
     private final String datoen;
     private final String stedet;
     private final double lengden;
     private final double vekten;
+    private String idn;
 
-    public Gjenfangst(String dato, String sted, double lengde, double vekt) {
+    public Gjenfangst(String id, String dato, String sted, double lengde, double vekt) {
+        idn = id;
         datoen = dato;
         stedet = sted;
         lengden = lengde;
         vekten = vekt;
+    }
+
+    public static ArrayList<Gjenfangst> getGjenfangst() {
+        return listeGjenfangst;
+    }
+
+    public String hentId() {
+        return idn;
     }
 
     public String toString() {
