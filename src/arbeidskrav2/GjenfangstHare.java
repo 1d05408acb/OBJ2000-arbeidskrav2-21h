2bcfceb2f6cadc28375f03c8399459b8b8a78904
Bjørnar Borge <dev.b.borge@gmail.com>
2021-11-05 21:03:41 +0100

    Update GjenfangstHare.java

--- src/arbeidskrav2/GjenfangstHare.java
+++ src/arbeidskrav2/GjenfangstHare.java
@@ -2,13 +2,15 @@ package arbeidskrav2;
 
 public class GjenfangstHare extends Gjenfangst {
     private final char fargen;
+    // Lage en sub klasse av klassen gjenfangst
 
-    public GjenfangstHare(String dato, String sted, double lengde, double vekt, char farge) {
-        super(dato, sted, lengde, vekt);
+    public GjenfangstHare(String id,String dato, String sted, double lengde, double vekt, char farge) {
+        super(id, dato, sted, lengde, vekt);
         fargen = farge;
     }
+
+    @Override
     public String toString() {
         return super.toString()  + "Fargen:" + fargen;
     }
-
 }
