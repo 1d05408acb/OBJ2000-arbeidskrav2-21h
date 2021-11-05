2c041a787074952ff7ca11e00d1f8f1fc3ae91a5
Bjørnar Borge <dev.b.borge@gmail.com>
2021-11-05 21:03:44 +0100

    Update Kontroll.java

--- src/arbeidskrav2/Kontroll.java
+++ src/arbeidskrav2/Kontroll.java
@@ -2,47 +2,68 @@ package arbeidskrav2;
 
 import javax.swing.*;
 import java.util.ArrayList;
+import java.util.Collections;
 
 public class Kontroll {
     // Opprette en array liste
-    ArrayList<Dyr> listeDyr = new ArrayList<>();
+    private final ArrayList<Dyr> listeDyr = new ArrayList<>();
+    ArrayList<Gjenfangst> listeGjenfangst = Gjenfangst.getGjenfangst();
+
     // Opprette variabler for å kunne telle
     private int antallGauper = 0;
     private int antallHarer = 0;
 
     public void registrerGaupe(String kjonn, double lengde, double vekt, String sted, String dato, double lengdeOre){
-        antallGauper++; // Hver gang det loopes igjennom så legges til ett tall til variabelen
-        String id = "G" + antallGauper;
-        Gaupe nyGaupe = new Gaupe(id, kjonn, lengde, vekt, sted, dato, lengdeOre);
+        antallGauper++; // //Autogenererer nytt nr for hver registrering
+        String id = "G" + antallGauper; // Lager id satt sammen av kjennetegn og nytt nr
+        Gaupe nyGaupe = new Gaupe(id, kjonn, lengde, vekt, sted, dato, lengdeOre); // Sender variablene videre til sub klassen gaupe av dyr
         listeDyr.add(nyGaupe);
     }
 
     public void registrerHare(String kjonn, double lengde, double vekt, String sted, String dato, char type_formatert, char farge_formatert) {
-        antallHarer++;
-        String id = "H" + antallHarer;
-        Hare nyHare = new Hare(id, kjonn, lengde, vekt, sted, dato, type_formatert, farge_formatert);
-        listeDyr.add(nyHare);
+        antallHarer++; //Autogenererer nytt nr for hver registrering
+        String id = "H" + antallHarer; // Lager id satt sammen av kjennetegn og nytt nr
+        Hare nyHare = new Hare(id, kjonn, lengde, vekt, sted, dato, type_formatert, farge_formatert); // Sender variablene videre til sub klassen hare av dyr
+        listeDyr.add(nyHare); // Sender variablene videre til sub klassen gaupe av dyr
     }
 
-    public Dyr finnDyr(String id) {
-        // Linjært søk etter dyr:
-        Dyr dyr = null;
+    public Gjenfangst finnDyrLinjaert(String id) {
+        /* Linjært søk med dyr id som parameter. Sliter med å få denne til å ungere ammen med binært.
+        Planen var at binært søket nedenfor skulle hente ut alle id registrert i listeDyr og at
+        linjær søket skulle hente ut alle fra listeGjenfangst. Men måtte bare gi opp og levere arbeidskravet*/
+        Gjenfangst gjenfangst = null;
         boolean funnet = false;
         int teller = 0;
-        while(!funnet) {
-            dyr = listeDyr.get(teller);
-            if(id.equals(dyr.hentId()))
+        while (!funnet) {
+            gjenfangst = listeGjenfangst.get(teller);
+            if (id.equals(gjenfangst.hentId())) {
                 funnet = true;
-            else
-                teller ++;
+            }
+            else {
+                teller++;
+            }
         }
-        return dyr;
+        return gjenfangst;
+    }
+
+
+    public Dyr finnDyrBinaert(String id) {
+        // Binært søk etter dyr med parameter id. Bruker malen som vi har gått igjennom i forelesning.
+        Collections.sort(listeDyr);
+        Dyr dummy = new Dyr(id, null, 0, 0, null, null);
+        int indeks = Collections.binarySearch(listeDyr, dummy);
+        if(indeks >= 0) {
+            return listeDyr.get(indeks);
+        }
+        else {
+            return null;
+        }
     }
 
     public void registrerGjenfangstGaupe(String id, String dato, String sted, double lengde, double vekt, double lengdeOre) {
-        Gaupe gaupen = (Gaupe)finnDyr(id);
+        Gaupe gaupen = (Gaupe)finnDyrBinaert(id);
         if (gaupen != null) {
-            GjenfangstGaupe Gjenfangst = new GjenfangstGaupe(dato, sted, lengde, vekt, lengdeOre);
+            GjenfangstGaupe Gjenfangst = new GjenfangstGaupe(id, dato, sted, lengde, vekt, lengdeOre);
             gaupen.registrerGjenfangst(Gjenfangst);
         } else {
             JOptionPane.showMessageDialog(null, "Det er ingen gaupe registrert med den ID'en");
@@ -50,16 +71,16 @@ public class Kontroll {
     }
 
     public void registrerGjenfangstHare(String id, String dato, String sted, double lengde, double vekt, char farge) {
-        Hare haren = (Hare)finnDyr(id);
+        Hare haren = (Hare)finnDyrBinaert(id);
         if (haren != null) {
-            GjenfangstHare Gjenfangst = new GjenfangstHare(dato, sted, lengde, vekt, farge);
+            GjenfangstHare Gjenfangst = new GjenfangstHare(id, dato, sted, lengde, vekt, farge);
             haren.registrerGjenfangst(Gjenfangst);
         } else {
             JOptionPane.showMessageDialog(null, "Det er ingen hare registrert med den ID'en");
         }
     }
-
-    public ArrayList<Dyr>  getDyr() {
+    // Henter ut listen listeDyr med get'er og return.
+    public ArrayList<Dyr> getDyr() {
         return listeDyr;
     }
 }
